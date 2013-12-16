0805c5badfacc55c9ad44acc7feaacb9c1d95e59
Yevgeny Lezhnin <z_lezhnin@mail2000.ru>
2013-12-17 01:17:55 +0700

    spline first degree

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 SOURCEOBJS = spline.cpp PolStr.o 
 main :
-	g++ -o lab4 $(SOURCEOBJS) 
+	g++ -o lab6 $(SOURCEOBJS) 
+main-32 :
+	g++ -o lab6 spline.cpp PolStr32.o
 main-debug :
-	g++ -g -O1 -o lab4 $(SOURCEOBJS)
+	g++ -g -O1 -o lab6 $(SOURCEOBJS)
--- spline.cpp
+++ spline.cpp
@@ -6,10 +6,8 @@
 #include <stdexcept>
 #include <vector>
 using namespace std;
-void solve(double*, double *y, int n, int, double**);
-double eval(double *result, int n, double xres);
-double a,b; //– границы отрезка (при равномерной сетке);
-double h;
+void solve(double *x, double *y, int n, int k, double **l);
+double eval(double *x, double **result, int n, int k, double xres);
 int main(int argc, char **argv) {
     try {
         int k; // порядок сплайна (1 – линейный, 2 – параболический, 3 – кубический)
@@ -49,10 +47,10 @@ int main(int argc, char **argv) {
         double *resnum = new double[m+1];
         for (int i=0; i<m+1; i++) {
         
-            double resT=eval(result,n,xres[i]);
+            double resT=eval(x,result,n,k,xres[i]);
             resnum[i] = resT;
             
-            cout << "x" << i << " P" << "(" << xres[i] << ") = " << resT << endl;
+            cout << "x" << i << " S" << "(" << xres[i] << ") = " << resT << endl;
         }
         if (t=="y") {
                 double *resfuncnum = new double[m+1];
@@ -64,6 +62,9 @@ int main(int argc, char **argv) {
                 sum /= m+1;
                 cout << "Deviation: " << sum << endl;
                 delete[] resfuncnum;
+        }
+        for (int i=0; i<n; i++) {
+            delete[] result[i];
         }
         delete[] result;
         delete[] resnum;
@@ -78,122 +79,29 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-double findMultValue(int n, vector<int> &indexes, int k) {
-    if (k==0) {
-        return 1;
-    }
-    bool *aUsing = new bool[n];
-    for (int i=0; i<n; i++) {
-        aUsing[i] = 1;
-    }
-    for (vector<int>::iterator iter=indexes.begin(); iter!=indexes.end(); ++iter) {
-        aUsing[*iter] = 0;
-    }
+void solve(double *x, double *y, int n, int k, double **l) {
     if (k==1) {
-        double sum = 0;
         for (int i=0; i<n; i++) {
-            if (aUsing[i]) {
-                sum -= x(i);
-            }
+            double a = y[i];
+            double b = (y[i+1]-y[i])/(x[i+1]-x[i]);
+            l[i][0] = b;
+            l[i][1] = a-b*x[i];
+            cout << a << ' ' << b << endl;
         }
-        delete[] aUsing;
-        return sum;
-    }
-    else {
-        int iusing = 0;
-        double sum = 0;
-        int count = 0;
-        int inum = n-indexes.size();
-        for (int i=0; i<inum; i++) {
-            while (!aUsing[iusing]) ++iusing;
-            indexes.push_back(iusing);
-            sum -= x(iusing)*findMultValue(n,indexes,k-1);
-            ++iusing;
-            ++count;
-        }
-        indexes.erase(indexes.end()-count,indexes.end());
-        return sum;
     }
 }
 
-void solve(double *x, double *y, int n, int k, double *l) {
-    if (k==1) {
-        for (int i=0; i<n; i++) {
-            l[i][1] = (y[i+1]-y[i])/(x[i+1]-x[i]);
-            l[i][0] = y[i]-l[i][1]*x[i];
-        }
+double eval(double *x, double **result, int n, int k, double xres) {
+    int i = 0;
+    for (; i<n; i++) {
+        if (xres<x[i+1] || i==n-1)
+            break;
     }
-    return;
-    /*for (int k=0; k<n; k++) {
-        l[k] = 0;
-    }
-    for (int i=0; i<n; i++) {
-        double denom = 1;
-        
-        if(s!="e")
-        {
-                for (int j=0; j<n; j++) {
-                    if (i!=j) {
-                        denom *= x(i)-x(j);
-                    }
-                }
-        }
-        else
-        {
-                denom =fact(i)*fact(n-i-1)/pow(-1,n-i-1);
-        }
-        double mult = y[i]/denom;
-        vector<int> indexes; //исключаемые индексы
-        indexes.push_back(i);
-        if (iDer==0) {
-            for (int k=0; k<n; k++) {
-                l[k] += mult*findMultValue(n,indexes,k);
-            }
-        }
-        else if (iDer==1) {
-            for (int j=0; j<n; j++) {
-                if (j==i) continue;
-                int il = 0;
-                indexes.push_back(j);
-                for (int k=0; k<n; k++) {
-                    if (k!=j) {
-                        l[il+iDer] += mult*findMultValue(n,indexes,il);
-                        ++il;
-                    }
-                }
-                indexes.pop_back();
-            }
-        }
-        else if (iDer==2) {
-            for (int m=0; m<n; m++) {
-                if (m==i) continue;
-                indexes.push_back(m);
-                for (int j=0; j<n; j++) {
-                    if (j==i || j==m) continue;
-                    int il = 0;
-                    indexes.push_back(j);
-                    for (int k=0; k<n; k++) {
-                        if (k!=j && k!=m) {
-                            l[il+iDer] += mult*findMultValue(n,indexes,il);
-                            ++il;
-                        }
-                    }
-                    indexes.pop_back();
-                }
-                indexes.pop_back();
-            }
-        }
-    }
-    for (int i=0; i<n; i++) {
-        cout << l[i] <<' ';
-    }
-    cout << endl;*/
-}
-
-double eval(double *result, int n, double xres) {
+    int index_in_spline = i;
+    cerr << "index in spline : " << i << endl;
     double sum = 0;
-    for (int i=0; i<n; i++) {
-        sum += result[i]*pow(xres,n-1-i);
+    for (i=0; i<k+1; i++) {
+        sum += result[index_in_spline][i]*pow(xres,k-i);
     }
     return sum;
 }
